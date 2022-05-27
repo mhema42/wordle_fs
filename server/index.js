37c291af8eda5c9cd1090476326185a74d3a2823
Mats Hemå <89969770+mhema42@users.noreply.github.com>
2022-05-27 14:07:08 +0200

    fix correct route for react build

--- server/index.js
+++ server/index.js
@@ -3,17 +3,7 @@ import fs from "fs/promises";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-    const filebuf = await fs.readFile("./public/index.html");
-    res.type("html");
-    res.send(filebuf);
-});
-
-app.get("/wordle", async (req, res) => {
-    const filebuf = await fs.readFile("../client/build/index.html");
-    res.type("html");
-    res.send(filebuf);
-});
+app.use(express.static("../client/build/"));
 
 app.get("/info", async (req, res) => {
     const filebuf = await fs.readFile("./public/info.html");
@@ -25,19 +15,6 @@ app.get("/highscore", async (req, res) => {
     const filebuf = await fs.readFile("./public/highscore.html");
     res.type("html");
     res.send(filebuf);
-});
-
-app.get("/*", async (req, res) => {
-    try {
-        const fileName = req.path;
-        console.log(fileName);
-        const filebuf = await fs.readFile(`./src/${fileName}`);
-        const type = fileName.split(".")[1];
-        res.type(type); 
-        res.send(filebuf);
-    } catch (err) {
-        res.status(404).end();
-    }
 });
 
 app.listen(5080);
